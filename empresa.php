9ebd190f1a2f9767acfb7fd43a16f496d859b459
Celio Barros <celio1301@gmail.com>
2019-12-05 18:25:05 -0300

    CRUD - resolvido

--- empresa.php
+++ empresa.php
@@ -4,16 +4,41 @@
     $empresaPDO = new EmpresaPDO();
         
     # Preencher Formulario com os dados 
-    if (isset($_GET['status']))
+        
+    if (isset($_GET['status'] ))
     {
         $acao=$_GET['status'];
         $codigo=$_GET['id'];
         $registro=$empresaPDO->busca($codigo);
+        
     }
-    else
+    else if( !isset($_GET['status']))
     {
         # Preencher o DataTable
         $empresas=$empresaPDO->lista("");
     }
-
+    
+    # Verificar operacoes de Banco
+    if ( isset($_POST['operacao']))
+    {
+        $operacao=$_POST['operacao'];
+        $codigo=$_POST['id'];
+        $descricao=$_POST['desEmp'];
+        switch ($operacao)
+        {
+            case 'a':
+                $registro=$empresaPDO->update($codigo,$descricao);
+            break;
+            case 'i':
+                $registro=$empresaPDO->insert($codigo,$descricao);
+            break;
+            case 'e':
+                echo "<p>Deletando..</p>";
+                $registro=$empresaPDO->delete($codigo);
+            break;
+        }
+        header("location:sisarq.php?option=empresa");
+    }
+     
+    
 ?>
--- empresaPDO.php
+++ empresaPDO.php
@@ -27,16 +27,36 @@ class EmpresaPDO
             
 
     }
-    public function insert($descricao)
+    public function insert($codigo,$descricao)
     {
+        /*
+        * Objeto: Incluir Empresa
+        * Parametros:   $codigo->Codigo da Empesa
+        *               $descricao->nome da empresa             
+        * Nota: Se o codigo da empresa for igual a 000, sistema deve gerar automaticamente o proximo codigo
+        */
         $sql='INSERT INTO tb_empresas(`cod_empresa`,`des_empresa`) ';
         $sql.= 'VALUES ( ';
-        $sql.='(SELECT right(concat("00",max(empresa.cod_empresa)+1),3) from tb_empresas empresa),';
-        $sql.='?)';
-        
-        $smtm=$this->conexao->prepare($sql);
-        $smtm->bindValue(1,$descricao);
+        if ($codigo=="000")
+        {
+            $sql.='(SELECT right(concat("00",max(empresa.cod_empresa)+1),3) from tb_empresas empresa),';
+            $sql.='?)';
+        }
+        else
+        {
+            $sql.='?,?)';
+        }
 
+        $smtm=$this->conexao->prepare($sql);
+        if ($codigo=="000")
+        {
+                $smtm->bindValue(1,$descricao);
+        }
+        else
+        {
+            $smtm->bindValue(1,$codigo);
+            $smtm->bindValue(2,$descricao);
+        }
         return $smtm->execute();
     }
 
@@ -49,18 +69,18 @@ class EmpresaPDO
         $smtm=$this->conexao->prepare($sql);
         $smtm->bindValue(1,$descricao);
         $smtm->bindValue(2,$codigo);
-
         return $smtm->execute();
     }
 
     public function delete($codigo)
     {
-        $sql="DELETE  tb_empresas ";
-        $sql.= " WHERE cod_empresa=?";
+        $sql="DELETE  FROM  tb_empresas ";
+        $sql.= " WHERE cod_empresa=? ";
         
         $smtm=$this->conexao->prepare($sql);
         $smtm->bindValue(1,$codigo);
-
+        
+        
         return $smtm->execute();
     }
 
